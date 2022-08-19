27f2be743a5c1e2861ec23e4f1a7e02c46fcdc6f
blueback <blueback28@gmail.com>
2022-08-19 21:59:48 +0900

    整理

--- BlueBackPad/Assets/UPM/Runtime/BlueBack/Pad/Version.cs
+++ BlueBackPad/Assets/UPM/Runtime/BlueBack/Pad/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Pad
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.24";
+		public const string packageversion = "0.0.25";
 
 		/** GetPackageVersion
 		*/
